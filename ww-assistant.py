3dc4a453aa0f83c619a55ad667bdaf5a130ce409
Maika Kanaka <maika-kanaka@protonmail.com>
2020-11-18 22:44:15 +0700

    tambah opsi rewrite vhost untuk ganti thema

--- ww-assistant.py
+++ ww-assistant.py
@@ -9,7 +9,7 @@ DB = mysqlcon.connect(host="localhost", user="root", password="", database="gues
 # path = "demos.migunesia.com/wedinv-999"
 # event_id = 3
 # python3 ww-assistant.py vhost --domain=theweddingofcitrabayu.xyz --path=demos.migunesia.com/wedinv-999 --event_id=
-def vhost(domain, path, event_id):
+def vhost(domain, path, event_id, rewrite = False):
     # update database
     sqlcursor = DB.cursor()
     sqlcursor.execute("UPDATE web_preferences SET pref_value = %s WHERE pref_group = 'general' AND pref_key = 'link' AND event_id = %s", ("http://" + domain, event_id))
@@ -29,8 +29,13 @@ def vhost(domain, path, event_id):
     vfile.write(vfilestr.strip())
 
     # reload apache2
-    subprocess.call(["a2ensite", domain])
-    subprocess.call(["systemctl", "reload", "apache2"])
+    if rewrite:
+        subprocess.call(["a2dissite", domain])
+        subprocess.call(["a2ensite", domain])
+        subprocess.call(["systemctl", "reload", "apache2"])
+    else:
+        subprocess.call(["a2ensite", domain])
+        subprocess.call(["systemctl", "reload", "apache2"])
 
 if __name__ == '__main__':
     fire.Fire()
