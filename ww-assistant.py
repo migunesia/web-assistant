bbe4630cef6b861d44574a0fcaa89d9588dc9061
Maika Kanaka <maika-kanaka@protonmail.com>
2021-09-24 10:48:41 +0700

    parameter protocol

--- ww-assistant.py
+++ ww-assistant.py
@@ -4,15 +4,15 @@ import subprocess
 
 DB = mysqlcon.connect(host="localhost", user="root", password="", database="guestbook_portal")
 
-# EXAMPLE TO USE 
+# EXAMPLE TO USE .
 # domain = "irwanyukino-wedding.xyz"
 # path = "demos.migunesia.com/wedinv-999"
 # event_id = 3
 # python3 ww-assistant.py vhost --domain=theweddingofcitrabayu.xyz --path=demos.migunesia.com/wedinv-999 --event_id=
-def vhost(domain, path, event_id, rewrite = False):
+def vhost(domain, path, event_id, rewrite = False, protocol = 'http'):
     # update database
     sqlcursor = DB.cursor()
-    sqlcursor.execute("UPDATE web_preferences SET pref_value = %s WHERE pref_group = 'general' AND pref_key = 'link' AND event_id = %s", ("http://" + domain, event_id))
+    sqlcursor.execute("UPDATE web_preferences SET pref_value = %s WHERE pref_group = 'general' AND pref_key = 'link' AND event_id = %s", (protocol + "://" + domain, event_id))
     DB.commit()
 
     # create file virtual host for ubuntu 
